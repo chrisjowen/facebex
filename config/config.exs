1d7552a1778c2283f2ba716b2bc9829579694d78
Ben Carter <bcarter@redventures.com>
2015-03-30 18:50:24 -0400

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,8 @@ use Mix.Config
 
 config :facebex, :security, 
 	storage_path:  "/var/tmp/config.dict.bin",
-	userAccessToken: "CAAL02WdqN2EBADj3uy3YbZASPQcGfXJR4pOgkX8X7yJyvvqyHG17n"
-	<> "cLPrXO3ev00Cng1xjPWz9H82PxgX00OJnj5ihSR1sq48AsTvIlGHqcXGc0QbdnT3uofM2XxEQo9lYf1YsrGyRLSx"
-	<> "0EzYpvLVVTUph0NX5gyVRF7ueiH8zNnT74MnqhneL9w5XNjFQMW2RPk7pjWDi7bq0qVRS",
+	userAccessToken: "fakeToken",
 	appId: "832164533516129",
-	appSecret: "12205a971859c0ef78ce31931e4ff61a"
+	appSecret: "fakeId"
 
 	

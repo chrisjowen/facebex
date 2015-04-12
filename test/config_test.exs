3de2b2f2a780726aa4f9d827ed53ebc7d90e6207
bcarter <bcarter@precise64.(none)>
2015-04-12 01:56:59 -0400

    added test for groups and fixed tests on config setup

--- test/config_test.exs
+++ test/config_test.exs
@@ -1,32 +1,23 @@
 defmodule ConfigTest do
     use ExUnit.Case
-    setup do
-        Application.delete_env(:facebex, :graphApiVersion)
-    end
-
-    test "default config path is empty" do 
-        assert Application.get_env(:facebex, :security) == "/tmp/config.dict.bin"
-    end
 
     test "version defaults to 2.2" do
         assert Facebex.Config.version == "2.2"
     end
 
-    test "version can be overriden" do
-        Application.put_env(:facebex, :graphApiVersion, "2.0")
-        assert Facebex.Config.version == "2.0"
+    test "setting a config option" do
+        Facebex.Config.set(:process, [foo: "bar"])
+        assert Process.get(:security) == [foo: "bar"]
+
+        assert "bar" === Facebex.Config.get(:process, :foo)
     end
 
     test "persist a token to disk works dext" do
-        Application.delete_env(:facebex, :security)
 
-        Facebex.Config.configure(:token, "12345678910")
-        assert :ok == Facebex.Config.persist(:token)
+        Facebex.Config.set_one(:process, :token, "12345678910")
+        assert :ok == Facebex.Config.persist(:process, :token)
 
-        Application.delete_env(:facebex, :security)
-
-        Facebex.Config.load
-        assert Facebex.Config.get(:token) == {:ok, "12345678910"}
+        Facebex.Config.load(:process)
+        assert Facebex.Config.get(:process, :token) == "12345678910"
     end
-
 end
--- test/facebook_sdk_test.exs
+++ test/facebook_sdk_test.exs
@@ -1,51 +1,47 @@
 defmodule FacebexTest do
   use ExUnit.Case
-  setup do
-        Application.delete_env(:facebex, :security)
-  end
 
   test "test our application is started" do
     assert  {:error, {:already_started, :facebex}} == Application.start(:facebex)
   end
 
-  test "test we can set security settings using the application" do
-    assert Facebex.configure([
-        appId: 12331,
-        appSecret: "ab123b2d123411"
-    ]) == :ok
-  end
-
-  test "test we can get security settigns using application" do
-        Facebex.configure([
+  test "test we can set and get security settings" do
+        Facebex.configure(:process, [
             appId: 12331,
             appSecret: "ab123b2d123411"
         ])
-        assert Facebex.configure == {:ok, [appId: 12331, appSecret: "ab123b2d123411"]}
+        assert Facebex.configure == [appId: 12331, appSecret: "ab123b2d123411"]
   end
 
   test "we can add a access token to existing configuration" do
-        Facebex.configure([
+        Facebex.configure(:process, [
             appId: 12331,
             appSecret: "ab123b2d123411"
         ])
 
-        assert Facebex.configure(:accessToken, "123231-1232-232321-12332") == :ok
-        assert Facebex.configure == {:ok, [appId: 12331, appSecret: "ab123b2d123411", accessToken: "123231-1232-232321-12332"]}
+        Facebex.configure(:process, :accessToken, "123231-1232-232321-12332") 
+        assert Facebex.configure == [appId: 12331, appSecret: "ab123b2d123411", accessToken: "123231-1232-232321-12332"]
   end
 
   test "when setting a config setting that does not exist will lazy load keyword list" do
-        assert Facebex.configure(:accessToken, "123231-1232-232321-12332") == :ok
-        assert Facebex.configure == {:ok, [accessToken: "123231-1232-232321-12332"]}
+        Facebex.configure(:process, :accessToken, "123231-1232-232321-12332") 
+        assert Facebex.configure == [accessToken: "123231-1232-232321-12332"]
   end
 
   test "access value directly from get" do
-        Facebex.configure([
+        Facebex.configure(:process, [
             appId: 12331,
             appSecret: "ab123b2d123411"
         ])
 
-        assert Facebex.configure![:appId] == 12331
-        assert Facebex.configure![:accessToken] == nil
+        assert Facebex.configure(:process)[:appId] == 12331
+        assert Facebex.configure(:process)[:accessToken] == nil
+  end
+
+  test "test we can get a known group" do
+    result =  Facebex.group(1540432296222124)
+    assert "South Charlotte Buy/Sell Market" === result.name
+    assert  %{"id" => "10153012381515129", "name" => "Ben Carter"} === result.owner
   end
 
 end

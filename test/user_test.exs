491100de322b6ec112005e387cff85918c22a6b2
bcarter <bcarter@precise64.(none)>
2015-04-14 00:25:58 -0400

    test case for user test

---
+++ test/user_test.exs
@@ -0,0 +1,12 @@
+defmodule ConfigTest do
+  use ExUnit.Case
+
+  test "lookup Ben Carter" do
+   #IO.inspect Facebex.API.User.search_by_name("Ben Carter")
+  end
+
+  test "lookup with full details" do
+  	IO.inspect Facebex.API.User.search_by_name("Ben Carter", :full_user)
+  end
+
+end
